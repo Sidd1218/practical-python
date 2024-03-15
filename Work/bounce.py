ab7501c5fdd1f152442f2203a9547549a153ace7
Siddharth <siddharth.shanker@atimotors.com>
2024-03-15 08:40:38 +0530

    bounce problem solved

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+bounces = 11
+for bounce in range(0,bounces):
+    new_height = height * 0.6
+    height = new_height
+    print(round(new_height,4))
